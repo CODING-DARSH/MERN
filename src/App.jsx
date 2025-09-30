21739a97e6693fcc5ba1e8451db5170c5d803875
CODING-DARSH <darshvit20@gmail.com>
2025-09-30 11:40:46 +0530

    Inputdemo

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import { Statedemo1 } from './components/Statedemo1'
 import { Statedemo2 } from './components/Statedemo2'
 import { Statedemo3 } from './components/Statedemo3'
 import { Inputdemo1 } from './components/Inputdemo1'
+import { Inputdemo2 } from './components/Inputdemo2'
+import { Inputdemo3 } from './components/Inputdemo3'
 // import './App.css'
 
 function App() {
@@ -30,7 +32,9 @@ function App() {
     {/* <Statedemo1></Statedemo1> */}
     {/* <Statedemo2></Statedemo2> */}
     {/* <Statedemo3></Statedemo3> */}
-    <Inputdemo1></Inputdemo1>
+    {/* <Inputdemo1></Inputdemo1> */}
+    {/* <Inputdemo2>?</Inputdemo2> */}
+    <Inputdemo3></Inputdemo3>
   <Footer></Footer>
 
   </div>
--- src/components/Inputdemo2.jsx
+++ src/components/Inputdemo2.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Inputdemo2 = () => {
+  const[name,setName]=useState("")
+  const[colour,setColor]=useState("")
+  const[submitted,setSubmitted]=useState(false)
+  const submittedhandler=()=>{
+    setSubmitted(true)
+  }
   return (
-    <div>Inputdemo2</div>
+    <div style={{textAlign:"center"}}>
+      <h1>Innput demo2</h1>
+      <div>
+        <label>Name</label>
+        <input type="text" onChange={(event)=>{setName(event.target.value)}}/>
+      </div>
+      <div>
+        <label>Color</label>
+        <input type='color' onChange={(event)=>{setColor(event.target.value)}}></input>
+      </div>
+      <div>
+        <button onClick={()=>{submittedhandler()}}>Submit</button>
+      </div>
+      {
+        submitted &&<div style={{color:colour}}>
+          <h1>{name}</h1>
+          
+          </div>
+      }
+    </div>
   )
 }
